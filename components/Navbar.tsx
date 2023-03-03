b7c54a1c38a36774510dbfbcd80a1de69a756cc6
Tinotenda Mupasiri <mavrickj9@gmail.com>
2023-03-03 18:24:29 +0200

    nav menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,7 +23,7 @@ const NAV_ITEMS: Array<NavItem> = [
     page: "gallery",
   },
   {
-    label: "Contact",
+    label: "Contact Us",
     page: "contact",
   },
 ];
@@ -31,17 +31,51 @@ const NAV_ITEMS: Array<NavItem> = [
 const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
   return (
-    <header className="w-full mx-auto px-4 fixed top-0 z-50 shadow bg-white dark:border-b dark:border-stone-600">
+    <header className="w-full mx-auto pr-4 fixed top-0 z-50 shadow bg-white dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
         <div>
-          <div className="flex items-center ">
-            <Image src="/logo.jpg" alt="" width={200} height={200} />
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <div className="flex items-center p-0">
+              <Image src="/logo.jpg" alt="" width={200} height={200} priority />
+            </div>
+            <div className="md:hidden">
+              <button
+                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+              </button>
+            </div>
           </div>
         </div>
-        <div className="items-center font-jakata text-lg rounded-md font-bold justify-center space-y-12 md:flex md:space-x-12 md:space-y-0">
-          {NAV_ITEMS.map((item, idx) => {
-            return <a key={idx}>{item.label}</a>;
-          })}
+        <div>
+          <div
+            className={`flex-1 justify-self-center mt-3 mb-2 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <div className="items-center font-jakata text-lg rounded-md font-bold justify-center space-y-4 md:flex md:space-x-12 md:space-y-0">
+              {NAV_ITEMS.map((item, idx) => {
+                return (
+                  <Link
+                    key={idx}
+                    to={item.page}
+                    className={
+                      "block lg:inline-block"
+                    }
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onClick={() => setNavbar(!navbar)}
+                  >
+                    {item.label}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </header>
