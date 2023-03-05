fc67de52b7fcd465da7e91d741165a719bc8feb8
Tinotenda Mupasiri <mavrickj9@gmail.com>
2023-03-05 16:02:35 +0200

    whyus padding

--- components/WhyUs.tsx
+++ components/WhyUs.tsx
@@ -7,21 +7,25 @@ import { IoStorefrontSharp } from "react-icons/io5";
 const WhyUs = () => {
   return (
     <section className="sect" id="whyUs">
-      <div>
-        <h1>Why Us</h1>
-        <div>
-          <div>
-            <FaRegEdit />
-            <h2>Customize your home plan</h2>
+      <div className="my-12 pb-2 md:pt-16 md:pb-20">
+        <h1 className="text-center font-bold text-4xl">Why Us</h1>
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-center items-center md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-center">
+          <div className="md:w-1/3">
+            <div>
+            <FaRegEdit size={50} />
+            </div>
+            <h2 className="text-center text-2xl font-bold mb-6 md:text-center">Customize your home plan</h2>
             <p>
               We will work with you to make small or large changes so you get
               the house design of your dreams. Tailor your house blueprints with
               our modification service.
             </p>
           </div>
-          <div>
-            <HiOutlineCurrencyDollar />
-            <h2>Stay on budget / low price guarantee</h2>
+          <div className="md:w-1/3">
+            <div className="justify-center align-center">
+            <HiOutlineCurrencyDollar size={50} />
+            </div>
+            <h2 className="text-center text-2xl font-bold mb-6 md:text-center">Stay on budget / low price guarantee</h2>
             <p>
               Get a Cost to Build report for any house plan. We also offer a low
               price guarantee for home plans and will beat the competition’s
@@ -30,9 +34,11 @@ const WhyUs = () => {
               offers/discounts).
             </p>
           </div>
-          <div>
-            <IoStorefrontSharp />
-            <h2>Get personalized help with your house blueprints</h2>
+          <div className="md:w-1/3">
+            <div>
+            <IoStorefrontSharp size={50} />
+            </div>
+            <h2 className="text-center text-2xl font-bold mb-6 md:text-center">Get personalized help with your house blueprints</h2>
             <p>
               Shopping for house designs can feel overwhelming. Our experienced
               house blueprint experts are ready to help you find the house plans
